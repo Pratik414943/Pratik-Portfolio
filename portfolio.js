6b52e2ace185a43cd281a67471e5ea442ecdd92e
Pratik <Singhpratikrajput786@gamil.com>
2023-04-27 16:26:50 +0530

    init

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+let MenuBtn= document.getElementById('MenuBtn')
+
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classlist.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark ')
+})
+
+
+
+let typed = new Typed('.auto-input',{
+    strings:['Font-End Developer!','Arduino Programmer!','Java Developer!','Andriod Developer!'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
